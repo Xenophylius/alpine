cdc72e4143ba40a320513a456136179509fa7403
Xenophylius <sirxofficial@gmail.com>
2024-06-12 18:14:17 +0200

    front ajustement

--- app/page.js
+++ app/page.js
@@ -9,8 +9,8 @@ export default function Home() {
   return (
     <>
     <section className="container-fluid m-0 p-0 border-bottom border-2">
-      <Splide aria-label="My Favorite Images" className="z-0 position-relative">
-        <SplideSlide className="position-relative">
+      <Splide aria-label="My Favorite Images" className="z-0 position-relative m-0 p-0">
+        <SplideSlide className="position-relative m-0 p-0">
           <video className="video" autoplay="true" muted="true" loop="true"><source src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/banners/mobile/alpine-teasing3-video-a290-hp-mobile.mp4.asset.mp4/3288366b89.mp4" /></video>
           <div className="position-absolute top-50 start-0 translate-middle-y mx-5 px-5 text-light bw-bold ">
             <h3>A290</h3>
@@ -18,15 +18,15 @@ export default function Home() {
             <button className="btn btn-outline-light rounded-0 mt-4">RESTEZ INFORME</button>
           </div>
         </SplideSlide>
-        <SplideSlide className="position-relative">
-          <img style={{height : 100 + 'vh !important;', width : 100 + '%'}} src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/banners/desktop/alpine-autopromo-UK-008-desktop.jpg.ximg.largex2.webp/50561ac9a8.webp" alt="Image 1"/>
+        <SplideSlide className="position-relative m-0 p-0">
+          <img style={{height : 89 + 'vh !important;', width : 100 + '%'}} src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/banners/desktop/alpine-autopromo-UK-008-desktop.jpg.ximg.largex2.webp/50561ac9a8.webp" alt="Image 1"/>
           <div className="position-absolute top-50 start-0 translate-middle-y mx-5 px-5 text-light bw-bold ">
             <h3>A110 R TURINI</h3>
             <h6 className="mt-3">LA PLUS RADICALE DES A110</h6>
             <button className="btn btn-light rounded-0 mt-4">DECOUVRIR</button>
           </div>
         </SplideSlide>
-        <SplideSlide className="position-relative">
+        <SplideSlide className="position-relative m-0 p-0">
           <video className="video" autoplay="true" muted="true" loop="true"><source src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/banners/mobile/alpine-teasing2-video-alpenglow-hp-mobile.mp4.asset.mp4/93bcfc02ed.mp4" /></video>
           <div className="position-absolute top-50 start-0 translate-middle-y mx-5 px-5 text-light bw-bold ">
             <h3>ALPENGLOW</h3>
@@ -34,8 +34,8 @@ export default function Home() {
             <button className="btn btn-outline-light rounded-0 mt-4">DECOUVRIR</button>
           </div>
         </SplideSlide>
-        <SplideSlide className="position-relative">
-          <img style={{height : 100 + 'vh !important;',  width : 100 + '%'}} src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/banners/desktop/alpine-A110s-hp-desktop.jpg.ximg.largex2.webp/87fab305ac.webp" alt="Image 2"/>
+        <SplideSlide className="position-relative m-0 p-0">
+          <img style={{height : 89 + 'vh !important;',  width : 100 + '%'}} src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/banners/desktop/alpine-A110s-hp-desktop.jpg.ximg.largex2.webp/87fab305ac.webp" alt="Image 2"/>
           <div className="position-absolute top-50 start-0 translate-middle-y mx-5 px-5 text-light bw-bold ">
             <h3>A110 S</h3>
             <h6 className="mt-3">SPORTIVITÉ INCARNÉE</h6>
@@ -75,7 +75,7 @@ export default function Home() {
       </div>
     </section>
 
-    <section className="bgBlack m-0 p-0 py-3">
+    <section className="bgBlack m-0 p-0 pt-3">
       <div className="row align-items-center ">
         <div className="col-6 text-light ps-5">
           <h1 className="fw-bold mb-3">FORMULE 1® </h1>
@@ -83,7 +83,7 @@ export default function Home() {
           <p className="btn btn-outline-light rounded-0 mt-4">DECOUVRIR</p>
           
         </div>
-        <div className="col-6">
+        <div className="col-6 p-0">
           <img className="height100Vh w-100" src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.xsmall.webp/965b4bd838.webp 375w, https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.small.webp/965b4bd838.webp 640w, https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.xsmallx2.webp/965b4bd838.webp 750w, https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.medium.webp/965b4bd838.webp 1024w, https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.large.webp/965b4bd838.webp 1280w, https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.mediumx2.webp/965b4bd838.webp 2048w, https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-005.jpg.ximg.largex2.webp/965b4bd838.webp 2560w" />
         </div>
 
@@ -91,17 +91,18 @@ export default function Home() {
 
     </section>
 
-    <section className="bgBlack m-0 p-0 py-3">
+    <section className="bgBlack m-0 p-0">
       <div className="row align-items-center ">
+      <div className="col-6 p-0">
+          <img className="height100Vh w-100" src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-006.jpg.ximg.xsmall.jpg/6eaf56de72.jpg" />
+        </div>
         <div className="col-6 text-light ps-5">
           <h1 className="fw-bold mb-3">WEC </h1>
           <p>Le savoir-faire technologique et design Alpine repousse les limites</p>
           <p className="btn btn-outline-light rounded-0 mt-4">DECOUVRIR</p>
           
         </div>
-        <div className="col-6">
-          <img className="height100Vh w-100" src="https://cdn.group.renault.com/alp/master/alpine-new-cars/homepage/editorial/homepage-2024/alpine-homepage-006.jpg.ximg.xsmall.jpg/6eaf56de72.jpg" />
-        </div>
+        
 
       </div>
 
