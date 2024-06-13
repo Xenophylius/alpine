197a32a2ad559f7917fabca390e78927b535c370
Xenophylius <sirxofficial@gmail.com>
2024-06-13 15:33:05 +0200

    vercel3

--- app/configurator/recap/page.js
+++ app/configurator/recap/page.js
@@ -1,37 +1,32 @@
 'use client'
 import { useAppSelector, useAppDispatch } from "@/lib/hook";
-import Carousel from "@/app/components/Carousel";
 import Ariane from "@/app/components/Ariane";
-import { calculPricing } from "@/lib/slice";
 
-export default function Recap() {
+export default function Recap() {  
 
-    const dispatch = useAppDispatch()
-    
-
-    const recap = useAppSelector((state) => state.counter.modelChoice)
+    const recapitulation = useAppSelector((state) => state.counter.modelChoice)
     const pricing = useAppSelector((state) => state.counter.pricing)
     let nameJantes = ''
-    if (recap.jante === 'blanc') {
+    if (recapitulation.jante === 'blanc') {
         nameJantes = 'Standard'
-    } else if (recap.jante === 'bleu'){
+    } else if (recapitulation.jante === 'bleu'){
         nameJantes = 'Serac'
-    } else if (recap.jante === 'noir'){
+    } else if (recapitulation.jante === 'noir'){
         nameJantes = 'legende'
     }
 
 
     let model =''
-    if (recap.name === 'pure') {
-        if (recap.color === 'blanc') {
+    if (recapitulation.name === 'pure') {
+        if (recapitulation.color === 'blanc') {
             model = "../images/model/pure/modele_pure-couleur_blanche-jante_standard (1).jpg"
-        } else if (recap.color === 'noir') {
-            model = "../images/model/pure/modele_pure-couleur_" + recap.color + "e-jante_standard (1).jpg"
+        } else if (recapitulation.color === 'noir') {
+            model = "../images/model/pure/modele_pure-couleur_" + recapitulation.color + "e-jante_standard (1).jpg"
         } else {
-            model = "../images/model/pure/modele_pure-couleur_" + recap.color + "-jante_standard (1).jpg"
+            model = "../images/model/pure/modele_pure-couleur_" + recapitulation.color + "-jante_standard (1).jpg"
         }
-    } else if (recap.name === 'legende') {
-        model = "../images/model/legende/modele_legende-couleur_" + recap.color + "-jante_legende-1.jpg"
+    } else if (recapitulation.name === 'legende') {
+        model = "../images/model/legende/modele_legende-couleur_" + recapitulation.color + "-jante_legende-1.jpg"
     }
 
 
@@ -42,13 +37,13 @@ export default function Recap() {
             <img src={model} className="col-10 rounded-2"/>
         </div>
         <div className="col-6 mt-5">
-        <p>Model : {recap.name} </p>
-        <p>Couleur  <img src={`../images/color/${recap.color}.jpg`} className="rounded-2 ms-2" style={{width: 50 + 'px'}}/></p>
+        <p>Model : {recapitulation.name} </p>
+        <p>Couleur  <img src={`../images/color/${recapitulation.color}.jpg`} className="rounded-2 ms-2" style={{width: 50 + 'px'}}/></p>
         <p>Jantes  <img src={`../images/jantes/jante-${nameJantes}.jpg`} className="rounded-2 ms-2" style={{width: 50 + 'px'}}/></p>
-        <p>Sellerie : {recap.sellerie}</p>
+        <p>Sellerie : {recapitulation.sellerie}</p>
         <p>Equipements  </p>
         <ul className="list-inline mx-auto">
-            {recap.equipement.map((equipement, index) => (
+            {recapitulation.equipement.map((equipement, index) => (
                 <li key={index} className="col-2 list-inline-item ">
                     <img src={`../images/equipements/${equipement}.jpg`} className="rounded-2 mt-2" style={{width: 100 + 'px'}}/>
                     <p>{equipement} </p>
